404c60a8a28c8deaac7170e859e9bc68a1e1141c
raja563 <shakyaraja1998@gmail.com>
2024-05-15 21:54:58 +0530

    all basic setting is completed.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,19 @@
 import express from "express";
 const app=express();
+import cors from "cors"
+// import cookieParser from "cookies-parser"
+app.use(cors());
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"));
+// app.use(cookieParser());
+
+
+
+//routes
+import userRouter from "./routes/user.routes.js"
+
+// routes declaration 
+app.use("/api/v1/users",userRouter);
 
 export {app};
